73049e3e28bc58b5c84550a4e9594b2a8ec274d2
姚庆奇 <yaoqngqi@bilibili.com>
2025-11-06 16:03:31 +0900

    finish

--- src/main/java/com/yao/ndn/convert/ConvertApplication.java
+++ src/main/java/com/yao/ndn/convert/ConvertApplication.java
@@ -2,12 +2,23 @@ package com.yao.ndn.convert;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class ConvertApplication {
-
+	private static final Logger log = LoggerFactory.getLogger(ConvertApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(ConvertApplication.class, args);
+		if (args.length < 1) {
+			log.error("Usage: java -jar demo.jar <client|server>");
+			System.exit(1);
+		}
+		if (args[0].equals("client")) {
+			NDNClient.main(args);
+		} else if (args[0].equals("server")) {
+			NDNServer.main(args);
+		}
 	}
 
 }
--- src/main/java/com/yao/ndn/convert/NDNServer.java
+++ src/main/java/com/yao/ndn/convert/NDNServer.java
@@ -1,5 +1,60 @@
 package com.yao.ndn.convert;
 
-public class NDNServer {
+import lombok.extern.slf4j.Slf4j;
+import net.named_data.jndn.Data;
+import net.named_data.jndn.Face;
+import net.named_data.jndn.Interest;
+import net.named_data.jndn.Name;
+import net.named_data.jndn.OnInterestCallback;
+import net.named_data.jndn.InterestFilter;
+import net.named_data.jndn.security.KeyChain;
+import net.named_data.jndn.security.identity.IdentityManager;
+import net.named_data.jndn.security.identity.MemoryIdentityStorage;
+import net.named_data.jndn.security.identity.MemoryPrivateKeyStorage;
+import net.named_data.jndn.security.policy.SelfVerifyPolicyManager;
+import net.named_data.jndn.util.Blob;
+import org.springframework.stereotype.Service; 
 
+@Slf4j
+@Service
+public class NDNServer {
+    public static void main(String[] args) {
+        Face face = new Face("localhost", 6363);
+        MemoryIdentityStorage memoryIdentityStorage = new MemoryIdentityStorage();
+        MemoryPrivateKeyStorage privateKeyStorage = new MemoryPrivateKeyStorage();
+
+        KeyChain keyChain = new KeyChain(new IdentityManager(memoryIdentityStorage, privateKeyStorage), 
+        new SelfVerifyPolicyManager(memoryIdentityStorage));
+        
+        try {
+            face.setCommandSigningInfo(keyChain, keyChain.getDefaultCertificateName());  
+            Name prefix = new Name("/yao/test/demo/B");
+            face.registerPrefix(prefix, new OnInterestCallback() {
+                @Override
+                public void onInterest(Name n, Interest i, Face f, long interestFilterId, InterestFilter filter) {
+                    try {
+                        log.info("Receive Interest: {}", i.getName().toUri());
+                        Name dataName = new Name(i.getName()).append("reply");
+                        Data data = new Data(dataName);
+                        data.setContent(new Blob("Congradulations!"));
+                        keyChain.sign(data);
+
+                        face.putData(data);
+                        log.info("Send data");
+
+                    } catch (Exception e) {
+                        log.error("Error handling interest", e);
+                    }
+                }
+            }, null);
+
+            for(int i = 0; i< 50; i++) {
+                face.processEvents();
+                Thread.sleep(5);
+            }
+
+        } catch (Exception e) {
+            log.error("Server init failed", e);
+        }
+    }
 }
